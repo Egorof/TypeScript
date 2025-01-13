63320e326780b27c879679b6820c3315b89cf9b1
Egor <egornagornov2009@gmail.com>
2025-01-13 18:31:53 +0300

    HatefulMonday

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 "use strict";
-const a = 1;
+var statusCode;
+(function (statusCode) {
+    statusCode["SUCCESS"] = "s";
+    statusCode["IN_PROCES"] = "p";
+    statusCode["FAILED"] = "f";
+})(statusCode || (statusCode = {}));
+const res = {
+    message: "Платеж успешеню",
+    statusCode: statusCode.SUCCESS,
+};
+if (res.statusCode === statusCode.SUCCESS) {
+}
+function action(status) {
+}
--- app.ts
+++ app.ts
@@ -1,4 +1,23 @@
-const a = 1;
- 
+enum statusCode {
+  SUCCESS = 's',
+  IN_PROCES = 'p',
+  FAILED = 'f',
+}
 
- 
+const res = {
+  message: "Платеж успешеню",
+  statusCode: statusCode.SUCCESS,
+};
+
+if (res.statusCode === statusCode.SUCCESS) {
+}
+
+
+function action(status: statusCode) {
+
+}
+
+
+const enum Roles{
+  ADMIN = 2
+}
