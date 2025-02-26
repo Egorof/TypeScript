a3535e4bc69289b26dd8cc991ac943a4b30fec1c
Egor <egornagornov2009@gmail.com>
2025-02-26 18:30:37 +0300

    Cool

--- app.ts
+++ app.ts
@@ -1,12 +1,8 @@
-class Venichle {
-  run: number;
+interface User {
+  name: string;
+  age: number;
+  email: string;
 }
 
-function kmToMIles<T extends Venichle>(venicle: T): T {
-  venicle.run = venicle.run / 0.62;
-  return venicle
-}
 
-console.log(kmToMIles({run: 55}));
-
-console.log('s')
+type P = Partial<User>
--- test.js
+++ test.js
@@ -1,2 +1,9 @@
 "use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInRlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
+function runTransaction(transaction) {
+    console.log();
+}
+const transaction = {
+    fromTo: ['1', '2']
+};
+runTransaction(transaction);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInRlc3QudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLFNBQVMsY0FBYyxDQUFDLFdBRXZCO0lBQ0MsT0FBTyxDQUFDLEdBQUcsRUFBRSxDQUFDO0FBQ2hCLENBQUM7QUFFRCxNQUFNLFdBQVcsR0FBdUM7SUFDdEQsTUFBTSxFQUFFLENBQUMsR0FBRyxFQUFFLEdBQUcsQ0FBQztDQUNuQixDQUFBO0FBRUQsY0FBYyxDQUFDLFdBQVcsQ0FBQyxDQUFBIn0=
